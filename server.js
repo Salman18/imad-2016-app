a4227b0faaf0996512ff622f6d7470f4674817ea
SALMAN KHAN <salmanaleemkhan786@gmail.com>
2016-09-18 02:12:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',fuction(req, res) {
+    res.('Article one requested and it will be served');
+});
+
+app.get('/article-two',fuction(req, res) {
+    res.send('Article two requested and it will be served');
+});
+
+app.get('/article-three',fuction(req, res) {
+    res.send('Article three requested and it will be served');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
