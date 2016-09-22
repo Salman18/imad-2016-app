91dcdf5872315863be0bec8f904f3015c2046c09
SALMAN KHAN <salmanaleemkhan786@gmail.com>
2016-09-22 23:32:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,9 +4,15 @@ var element = document.getElementById("main.text");
 
 element.innerHTML="HI,I am salman ,welocme to WEB APP";
 
-//move image
+//move the image
 var img=document.getElementById("madi");
-img.onclick=function(){
+var marginLeft=0;
+function moveRight() {
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function()
+{
 
-    img.style.marginLeft="100px";
+    var interval=setInterval(moveRight,100);
 };
